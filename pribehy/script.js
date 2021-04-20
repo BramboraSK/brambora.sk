39c2b4d99e26f204832a5952dcc315c33543d567
BramboraSK <majojo992@gmail.com>
2021-04-20 13:00:02 +0200

    ✨ Add pribehy

---
+++ pribehy/script.js
@@ -0,0 +1,53 @@
+const pribehy = [
+    "Keď sme boli u Normana, tak sme si povedali, že podpálime jeho tetu, lebo nám nevedel upiecť grilované penisy, so šípkovou omáčkou. Preto nás začal šikanovať takým veľkým, hrozným opaskom od značky Supreme Race. Jakub Prachař mu prišiel ujebať magickú osušku, ktorú sa snaží uhasiť. Horela. Následne ale ožila, zakašlala a zmizla. Honik divom začal behať okolo stoličky, potom okolo slnka, ale zhorel. \"Ale zomrel,\" povedal, \"huh.\" Potom si dal na svoje prsia striptérky piko. Bohužiaľ to nestačilo. Dal si tam hovno, ktoré voňalo. Brambora si prdol tak moc, že keď teta ucítila ten smrad, usrala si ešte viac. Honika zachránili lekári až po jeho babičke, ktorá smrdí. Napriek všetkému smradu si BramboraSK uvedomil podstatu vesmíru a hovna. Kokyslav nevedel, že aj keď Brambora verí v mimozemšťanov, je schopný srať. Preto E.T. Kokyho naučil, ako Honikovi vrátiť jeho život. Avšak Koky nepozná kamaráda Honika. Ten preto navždy Kokymu nič neodpisuje a nebude až do smrti.",
+    "Keď sme vstali z kaluže, lebo ano, kaluze se pridali do spolku proti ochranárom vody. Tak tedy jsme snědli chlebíček se zelím a česnekem. Potom sme utopili rybu v oleji a naftě, opekli sme olej ve vzduchu kanalizačnej roury, a najednou... Brambora odstranil kaluž a svoje nadávky uklidil stisknutím Normana. Norman neprotestoval, nechal sa mačkať až do konca Stmívání. Potom odešel na WC poslat svojho hnedáka Bramborovi, který nečakal nic menšieho, ale väčšieho tiež nie. Vonělo to jako mrazene býčí játra. Ochutnal to, \"PMFPGFJ\", treba osolit, pridať ovčí kopyta, možná taky kvapku dětského oleja. Dále upéct tátovo kuře na rožni, k tomu cibulovou omacku. Tak nakoniec skočil aj Norman. Nielen, že skočil, no aj strážil jeho profesionální kariéru poslíčka a svůj hezký, naleštěný, překrásný a malý vymyšlený kamarad  mu začal hladkať pipík, po celý čas hladkania mu hovoril: \"Oh, to je také smradlavé\". A potom si zobral velký čierny klacek, mrdnul ho po frekiemu, poděkoval Honikovi.",
+    "Jednou jsem si ukopl vlak a potom sa stalo to že vlak vykolejil do Normana. A jeho noha byla modrá az tak ze sa sfarbila na ruku a pokračovala smerom dole dokud nezastavila. Koky sa narodil, A umřel. ale potom zázračně reinkarnoval do jeho psa, který umrel. Potom si Jakub šel vzít xanax kvůli rýmě a nachlazení, proto umřel. Mal to dát až kdyż si prdl ale nemohl si pomoct. Musel zavolat Normanovi a říct mu: \"Miluju tě.\" Avšak ne, ale ne, myslím, myslim...  ...Mno, asi ne no, nejspíš si měl honit nad condorikem z papežom lebo sa posere. Frekie sa zabil. Koky bol polomrtvý a smrdel mu jeho nos. Ale nie, on sa umyl! Kto si ted usral? Bol to fakt smradlavy zápach, preto jsem si koupil plynovu komoru, pustil ji a nepomohlo mu to. Nakoniec umrely všichni.",
+    "Raz som urobil vo vane bublinu hnedu jak som sa posral. Na otcovi som sa pogrcal lebo som sa prežral z pizze. Protože môj žaludok je na pizzu alergický. Jelikož jsem to přehnal jak mexičan a mal som pocit, že chudnem, ale bol som tlstý ako Homer, zacal som športovať. Trvalo to krátko. Prestal som vcera. Neprestal jíst, ani kourit perník, trávu, alebo jest IPhony. Chutili velice železně, trochu jako hrdzavé versace kabelky. 200kg, myslel som, že jakub chce být jako já. A zistil, prekvapujúco, on už není. Tak skončil s IPhonmi, trávou. Začal znovu chlastat naftové oleje, ktoré si oblubil roku 2019 při velkej slavnosti, počas ktorej sa zamiloval do gucciho ponožek. Sadli si tak nějak vyborne kolem ohně, začali sa zpívat, tančit Ievan polkka a pridal sa aj Rebelko. Celý rád se postavil do pozoru, nadychol sa, vydychol, zasalutoval. Hitlera vysvätil Papež za kněze, Gianni protestoval, ale aj tleskal. Schizofrenik, Brambora, pomyslel si myšlenku: \"Smrdim.\" řekl a zmizol. Nevěděl, že skutočně bol len trochu chudý. Jakub dostal psie klobasy od Normana, ktory navrhol picu kabru, fajne jídlo. Odgrgol som si, lebo mám retardáciu žaludku ktorá mi spôsobila otravu.",
+    "Pánská toaleta bola veľmi očúraná, FiFqovi moc zle bolo, pretože očúraná miestnosť mu robila dobre. Koky uvidel FiFqa ako sere a ako honí a eakuluje. Koky mu oblízal toaleťák lebo sa neumí postaviť zo stoličky na mušlu. Honil ako panic 16-tymi narozeninami. Jebal hlavou Kokyho, dokud mu neutrhl panenskú blánu. Potom BramboraSK začal jebať klub japonských študentiek z pripravnej školy. Začali mu chodiť vyhrazně domov a  začali ho využívať ako hračičku. BramboraSK a holky sa opýtali Kokyho či sa nepridá do baletního klubu, ale Koky už nežije. FiFqo stále Kokyho líbal do tváře, ucha a kakaciku, ale prestal. Koky a Brambora FiFqovi stáhli trenky značky Versace vonící po semene, které roky usychali. Never zomrel. Možno. Never dýchá? \"Ano!\" Never ale nejakým zvláštnym spôsobom oživnul a nastúpil na pripravnu skolu a nasiel si holku. Tá ale ho pleskla a uklouzla do haremu. Sama. Za ňou prisiel Never a spadol za prah od KLUBU, který bol zavretý. Ale zrazu klub swaggerov prispel na vstupne do baletneho kruzku a na pivo smichovske dvanastku z mongolska. Pockat, mongolska? Nevadí, možno zkusíme skutočné pivo od smichovskych swaggerov.",
+];
+
+const datumy = [
+    "15.6.2019",
+    "16.6.2019",
+    "16.6.2019",
+    "17.6.2019",
+    "17.6.2019",
+];
+
+const pribehyDiv = document.getElementById("pribehy");
+for(let i = 0; i < pribehy.length; ++i) {
+    let pribeh = document.createElement("div");
+    pribeh.classList.add("pribeh");
+
+    let number = document.createElement("h1");
+    number.classList.add("number");
+    number.id = String(i + 1);
+    number.innerHTML = String(i + 1);
+    pribeh.appendChild(number);
+
+    let date = document.createElement("h3");
+    date.classList.add("date");
+    date.innerHTML = datumy[i];
+    pribeh.appendChild(date);
+
+    let text = document.createElement("p");
+    text.classList.add("text");
+    text.innerHTML = pribehy[i];
+    pribeh.appendChild(text);
+
+    let audio = document.createElement("audio");
+    audio.classList.add("audio");
+    audio.controls = true;
+    audio.src = `audio/${i + 1}.mp3`;
+    pribeh.appendChild(audio);
+
+    pribehyDiv.appendChild(pribeh);
+}
+
+const input = document.getElementById("jumpto");
+input.max = String(pribehy.length);
+
+const jumpTo = () => {
+    let number = document.getElementById("jumpto").value;
+    location.href = `#${number}`;
+}
